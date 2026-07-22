de84449dd167d46b15592f70a315ac86f2c359a7
fovlin <fovlin@163.com>
2026-07-22 18:12:38 +0000

    update

--- data/acovia/worldgen/configured_feature/get_all_flower.py
+++ data/acovia/worldgen/configured_feature/get_all_flower.py
@@ -4,6 +4,7 @@ flower_list_path = Path('./flower.list')
 flower_list = json.loads(flower_list_path.read_text())
 target_path = Path('./ancient_flower.json')
 target = json.loads(target_path.read_text())
+target['config']['to_place']['rules'][0]['then']['entries'] = []
 for flower in flower_list:
     if not flower.endswith('leaves'):
         data = {'data':{'Name':flower},'weight':100}
