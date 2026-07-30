639f663f1d7a914a409b72010026fe593c740a50
fovlin <fovlin@163.com>
2026-07-30 16:11:54 +0000

    fixed

--- data/acovia/worldgen/placed_feature/add_to_list.py
+++ data/acovia/worldgen/placed_feature/add_to_list.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 import json
+
 dir = Path('./')
-feature_list = []
+vegetal_list = []
+surface_list = []
+
 for child in dir.iterdir():
     if child.name.endswith('.json'):
-        feature_list.append('acovia:' + child.name.removesuffix('.json'))
+        if child.name.startswith("vegetal"):
+            vegetal_list.append('acovia:' + child.name.removesuffix('.json'))
+        if child.name.startswith("surface"):
+            surface_list.append('acovia:' + child.name.removesuffix('.json'))
+
 target = Path('../biome/ancient_forest.json')
 src_data = json.loads(target.read_text())
-src_data['features'][9] = feature_list
+src_data['features'][9] = vegetal_list
+src_data['features'][5] = vegetal_list
 target.write_text(json.dumps(src_data,indent=2))
